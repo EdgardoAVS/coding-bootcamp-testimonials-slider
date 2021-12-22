71324f02c9705096419e339946dcb3d6d5f50ba4
EdgardoAVS <edgardoovargass@gmail.com>
2021-12-22 17:00:54 -0500

    Fixed, reload page every time the next and previous buttons are clicked.

--- script.js
+++ script.js
@@ -7,26 +7,32 @@ const buttonNext = document.querySelectorAll('#button-next');
 
 slider.insertAdjacentElement('afterbegin', reviewContainerLast);
 
-function Next () {
+function Next (event) {
   let reviewContainerFirst = document.querySelectorAll('.review')[0];
   slider.style.marginLeft = '-100%';
+  event.preventDefault();
   setTimeout(function () {
     slider.insertAdjacentElement('beforeend', reviewContainerFirst);
     slider.style.marginLeft = '0%';
+ 
   }, 300);
+  
 };
 
-function Preview () {
+function Preview (event) {
   let reviewContainerFirst = document.querySelectorAll('.review')[0];
   slider.style.marginLeft = '-100%';
+  event.preventDefault();
   setTimeout(function () {
     slider.insertAdjacentElement('beforeend', reviewContainerFirst);
     slider.style.marginLeft = '0%';
   }, 300);
+
 }
 
 for(let i = 0; i < buttonNext.length; i++) {
   buttonNext[i].addEventListener('click', Next);
+  
 }
 for(let i = 0; i < buttonPreview.length; i++) {
   buttonPreview[i].addEventListener('click', Preview);
